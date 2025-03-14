5993a233fbffa0c6a3223b1a8ece62712902ff2e
zDefcon20 <antholozadarequelme@gmail.com>
2025-03-13 22:38:04 -0500

    Refactor input handling and enhance user prompts for name, age, and gender

--- EjercicioLogica2/Program.cs
+++ EjercicioLogica2/Program.cs
@@ -6,159 +6,205 @@ int Edad = 0;
 string sexo = "";
 string invalidMonto = "";
 string invalidEdad = "";
+string invalidSeguir = "";
 string nombre = "";
+int Seguir = 0;
+bool Error = true;
 do
 {
-    Console.WriteLine("\n----------BIENVENIDO-----------");
-    Console.WriteLine("Ingrese su Nombre");
-    Console.Write("-> ");
-    nombre = Console.ReadLine();
-    if (string.IsNullOrWhiteSpace(nombre) || nombre.Any(char.IsDigit))
+    do
     {
-        Console.WriteLine("Error: No debe contener números ni estar vacío. Por favor, inténtelo nuevamente.");
-        On = true;
-    }
-    else
-    {
-        bool continuar = true;
-        do
+        Console.WriteLine("\n----------BIENVENIDO-----------");
+        Console.WriteLine("Ingrese su Nombre");
+        Console.Write("-> ");
+        nombre = Console.ReadLine();
+        if (string.IsNullOrWhiteSpace(nombre) || nombre.Any(char.IsDigit))
         {
-            Console.WriteLine("Ingrese su Edad");
-            Console.Write("-> ");
-            invalidEdad = Console.ReadLine();
-            if (!Int32.TryParse(invalidEdad, out int edad))
+            Console.WriteLine("Error: No debe contener números ni estar vacío. Por favor, inténtelo nuevamente.");
+            On = true;
+        }
+        else
+        {
+            bool continuar = true;
+            do
             {
-                Console.WriteLine("\n----------------------");
-                Console.WriteLine("Error: Ingrese una Edad");
-                Console.WriteLine("-----------------------\n");
-                continuar = true;
-            }
-            else
-            {
-                Edad = edad;
-                Console.WriteLine("\n------------------------------");
-                Console.WriteLine("Datos Ingresados Correctamente");
-                Console.WriteLine("------------------------------\n");
-                bool salir = false;
-                do
+                Console.WriteLine("Ingrese su Edad");
+                Console.Write("-> ");
+                invalidEdad = Console.ReadLine();
+                if (!Int32.TryParse(invalidEdad, out int edad))
                 {
-                    Console.WriteLine("--------------------------------");
-                    Console.WriteLine("Ingrese su genero");
-                    Console.WriteLine("M = Masculino");
-                    Console.WriteLine("F = Femenino");
-                    Console.WriteLine("--------------------------------");
-                    Console.Write("-> ");
-                    sexo = Console.ReadLine();
-                    if (string.IsNullOrWhiteSpace(sexo) || sexo.Any(char.IsDigit))
+                    Console.WriteLine("\n----------------------");
+                    Console.WriteLine("Error: Ingrese una Edad");
+                    Console.WriteLine("-----------------------\n");
+                    continuar = true;
+                }
+                else
+                {
+                    Edad = edad;
+                    Console.WriteLine("\n------------------------------");
+                    Console.WriteLine("Datos Ingresados Correctamente");
+                    Console.WriteLine("------------------------------\n");
+                    bool salir = false;
+                    do
                     {
-                        Console.WriteLine(
-                            "\nError: No debe contener números ni estar vacío. Por favor, inténtelo nuevamente.\n");
-                        salir = false;
-                    }
-                    else if (sexo == "M" || sexo == "m" || sexo == "F" || sexo == "f")
-                    {
-                        Console.WriteLine("\n------------DATOS INGRESADOS CORRECTAMENTE------------");
-                        Console.WriteLine("-----------------------------------------------");
-                        Console.WriteLine("Nombre -> " + nombre);
-                        Console.WriteLine("Edad -> " + Edad);
-                        Console.WriteLine("Sexo -> " + sexo);
-                        Console.WriteLine("-----------------------------------------------\n");
-                        salir = true;
-                        continuar = false;
-                        On = false;
-                    }
-                    else
-                    {
-                        Console.WriteLine("\nError: Ingrese un genero");
-                        salir = false;
-                    }
-                } while (salir == false);
+                        Console.WriteLine("--------------------------------");
+                        Console.WriteLine("Ingrese su genero");
+                        Console.WriteLine("M = Masculino");
+                        Console.WriteLine("F = Femenino");
+                        Console.WriteLine("--------------------------------");
+                        Console.Write("-> ");
+                        sexo = Console.ReadLine();
+                        if (string.IsNullOrWhiteSpace(sexo) || sexo.Any(char.IsDigit))
+                        {
+                            Console.WriteLine(
+                                "\nError: No debe contener números ni estar vacío. Por favor, inténtelo nuevamente.\n");
+                            salir = false;
+                        }
+                        else if (sexo == "M" || sexo == "m" || sexo == "F" || sexo == "f")
+                        {
+                            Console.WriteLine("\n------------DATOS INGRESADOS CORRECTAMENTE------------");
+                            Console.WriteLine("-----------------------------------------------");
+                            Console.WriteLine("Nombre -> " + nombre);
+                            Console.WriteLine("Edad -> " + Edad);
+                            Console.WriteLine("Sexo -> " + sexo);
+                            Console.WriteLine("-----------------------------------------------\n");
+                            salir = true;
+                            continuar = false;
+                            On = false;
+                        }
+                        else
+                        {
+                            Console.WriteLine("\nError: Ingrese un genero");
+                            salir = false;
+                        }
+                    } while (salir == false);
+                }
+            } while (continuar == true);
+        }
+    } while (On == true);
+
+    do
+    {
+        Console.WriteLine("Ingrese el monto de su compra");
+        Console.Write("-> ");
+        invalidMonto = Console.ReadLine();
+
+        if (!float.TryParse(invalidMonto, out monto))
+        {
+            Console.WriteLine("\n--------------------------------------");
+            Console.WriteLine("Error: Debe Ingresar Un Monto Numerico");
+            Console.WriteLine("--------------------------------------\n");
+            On = true;
+        }
+        else if (monto <= 0)
+        {
+            Console.WriteLine("\n--------------------------------------------");
+            Console.WriteLine("Error: El monto no puede ser menor o igual a 0");
+            Console.WriteLine("----------------------------------------------\n");
+            On = true;
+        }
+        else
+        {
+            Console.WriteLine("\n--------------------------------------");
+            Console.WriteLine("Monto Ingresados Correctamente");
+            Console.WriteLine("----------------------------------------\n");
+
+            if (monto >= 300 && monto < 500)
+            {
+                descuento = monto * 0.30f; // porcentaje del descuento
+                total = monto - descuento;
+                montoFinal = total;
+                On = false;
             }
-        } while (continuar == true);
-    }
-} while (On == true);
+            else if (monto >= 500 && monto < 1000)
+            {
+                descuento = monto * 0.30f; // porcentaje del descuento
+                total = monto - descuento;
+                montoFinal = total;
+                On = false;
+            }
+            else if (monto < 300)
+            {
+                descuento = monto * 0.10f; // porcentaje del descuento
+                total = monto - descuento;
+                montoFinal = total;
+                On = false;
+            }
+        }
+    } while (On == true);
 
-do
-{
-    Console.WriteLine("Ingrese el monto de su compra");
-    Console.Write("-> ");
-    invalidMonto = Console.ReadLine();
-
-    if (!float.TryParse(invalidMonto, out monto))
+    float descuentoEdad = 0;
+    float montoEdadFinal = 0;
+    if ((sexo.Equals("F", StringComparison.OrdinalIgnoreCase)) && (Edad >= 50 && Edad <= 120))
     {
-        Console.WriteLine("\n--------------------------------------");
-        Console.WriteLine("Error: Debe Ingresar Un Monto Numerico");
-        Console.WriteLine("--------------------------------------\n");
-        On = true;
+        descuentoEdad = total * 0.10f;
+        montoEdadFinal = montoFinal - descuentoEdad;
+        Console.WriteLine("--------------------------------------------");
+        Console.WriteLine("MontoInicial: " + monto);
+        Console.WriteLine("Descuento: " + descuento);
+        Console.WriteLine("DescuentoEdad: " + descuentoEdad);
+        Console.WriteLine("Total a Pagar: " + montoEdadFinal);
+        Console.WriteLine("-----------------------------------------------");
     }
-    else if (monto <= 0)
+    else if (sexo.Equals("M", StringComparison.OrdinalIgnoreCase) && (Edad >= 60 && Edad <= 120))
     {
-        Console.WriteLine("\n--------------------------------------------");
-        Console.WriteLine("Error: El monto no puede ser menor o igual a 0");
-        Console.WriteLine("----------------------------------------------\n");
-        On = true;
+        descuentoEdad = total * 0.10f;
+        montoEdadFinal = montoFinal - descuentoEdad;
+        Console.WriteLine("--------------------------------------------");
+        Console.WriteLine("MontoInicial: " + monto);
+        Console.WriteLine("Descuento: " + descuento);
+        Console.WriteLine("DescuentoEdad: " + descuentoEdad);
+        Console.WriteLine("Total a Pagar: " + montoEdadFinal);
+        Console.WriteLine("-----------------------------------------------");
     }
     else
     {
-        Console.WriteLine("\n--------------------------------------");
-        Console.WriteLine("Monto Ingresados Correctamente");
-        Console.WriteLine("----------------------------------------\n");
-
-        if (monto >= 300 && monto < 500)
-        {
-            descuento = monto * 0.30f; // porcentaje del descuento
-            total = monto - descuento;
-            montoFinal = total;
-            On = false;
-        }
-        else if (monto >= 500 && monto < 1000)
-        {
-            descuento = monto * 0.30f; // porcentaje del descuento
-            total = monto - descuento;
-            montoFinal = total;
-            On = false;
-        }
-        else if (monto < 300)
-        {
-            descuento = monto * 0.10f; // porcentaje del descuento
-            total = monto - descuento;
-            montoFinal = total;
-            On = false;
-        }
+        Console.WriteLine("--------------------------------------------");
+        Console.WriteLine("MontoInicial: " + monto);
+        Console.WriteLine("Descuento: " + descuento);
+        Console.WriteLine("Total a Pagar: " + montoFinal);
+        Console.WriteLine("-----------------------------------------------");
     }
-} while (On == true);
 
-float descuentoEdad = 0;
-float montoEdadFinal = 0;
-if ((sexo.Equals("F", StringComparison.OrdinalIgnoreCase)) && (Edad >= 50 && Edad <= 120))
-{
-    descuentoEdad = total * 0.10f;
-    montoEdadFinal = montoFinal - descuentoEdad;
-    Console.WriteLine("--------------------------------------------");
-    Console.WriteLine("MontoInicial: " + monto);
-    Console.WriteLine("Descuento: " + descuento);
-    Console.WriteLine("DescuentoEdad: " + descuentoEdad);
-    Console.WriteLine("Total a Pagar: " + montoEdadFinal);
-    Console.WriteLine("-----------------------------------------------");
-}
-else if (sexo.Equals("M", StringComparison.OrdinalIgnoreCase) && (Edad >= 60 && Edad <= 120))
-{
-    descuentoEdad = total * 0.10f;
-    montoEdadFinal = montoFinal - descuentoEdad;
-    Console.WriteLine("--------------------------------------------");
-    Console.WriteLine("MontoInicial: " + monto);
-    Console.WriteLine("Descuento: " + descuento);
-    Console.WriteLine("DescuentoEdad: " + descuentoEdad);
-    Console.WriteLine("Total a Pagar: " + montoEdadFinal);
-    Console.WriteLine("-----------------------------------------------");
-}
-else
-{
-    Console.WriteLine("--------------------------------------------");
-    Console.WriteLine("MontoInicial: " + monto);
-    Console.WriteLine("Descuento: " + descuento);
-    Console.WriteLine("Total a Pagar: " + montoFinal);
-    Console.WriteLine("-----------------------------------------------");
-}
+    do
+    {
+        Console.WriteLine("\n-----------------------------------------------");
+        Console.WriteLine("Desea Continuar?");
+        Console.WriteLine("1 = Si");
+        Console.WriteLine("0 = No");
+        Console.WriteLine("-------------------------------------------------");
+        Console.Write("-> ");
+        invalidSeguir = Console.ReadLine();
+
+        if (!Int32.TryParse(invalidSeguir, out int seguir))
+        {
+            Console.WriteLine("\n-----------------------------------------------");
+            Console.WriteLine("Error: Debe Ingresar un valor numerico");
+            Console.WriteLine("-------------------------------------------------\n");
+            Error = true;
+        }
+
+        else if (seguir == 0)
+        {
+            Console.WriteLine("\n-----------------------------------------------");
+            Console.WriteLine("             Gracias por su compra");
+            Console.WriteLine("-------------------------------------------------");
+            Error = false;
+            Seguir = seguir;
+        }
+        else if (seguir == 1)
+        {
+            Console.Clear();
+            Error = false;
+            Seguir = seguir;
+        }
+        else
+        {
+            Console.WriteLine("Error: Ingrese un valor correcto");
+            Error = true;
+        }
+    } while (Error == true);
+
+} while (Seguir == 1);
 
 Console.ReadKey();
