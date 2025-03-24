29de645903ea1fbe3dc17e51b4eb926e366e127a
zDefcon20 <antholozadarequelme@gmail.com>
2025-03-24 02:14:36 -0500

    Add new project files and configuration for Array_1 targeting .NET 9.0

---
+++ Array_1/Program.cs
@@ -0,0 +1,36 @@
+﻿int nums = 0, i = 0;
+int [] numeros = new int[10];
+string invalidNums = "";
+
+while (i < numeros.Length) // desde 0 hasta 9
+{
+    Console.WriteLine("\nIndice["+i+"]");
+    Console.WriteLine("Ingrese un numero del 1 al 100");
+    Console.Write("-> ");
+    
+    if(!int.TryParse(Console.ReadLine(), out nums))
+    {
+        Console.WriteLine("\nIndice["+i+"]");
+        Console.WriteLine("Error: El valor ingresado no es un numero\n");
+        continue;
+    }
+    
+    else if (nums > 0 && nums <= 100)
+    {
+        Console.WriteLine("\nIndice["+i+"]");
+        Console.WriteLine("Se Ingreso Correctamente el numero: " + nums);
+        numeros[i] = nums;
+        i++; // incrementador del bucle while
+    }
+    else
+    {
+        Console.WriteLine("\nIndice["+i+"]");
+        Console.WriteLine("Error: El numero ingresado sale del limite del rango (1 - 100)\n");
+        if (i > 0 && i < numeros.Length)
+        {
+            i--; // decrementador del bucle while
+        }
+    }
+}
+
+
